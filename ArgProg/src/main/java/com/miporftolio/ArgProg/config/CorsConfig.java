c8452a5af801c8e3556707c669f01fe22523ac5b
askra <askrajpr@gmail.com>
2023-05-10 17:13:54 -0300

    Se agrego el dockerfile para su deploy en Render

--- ArgProg/src/main/java/com/miporftolio/ArgProg/config/CorsConfig.java
+++ ArgProg/src/main/java/com/miporftolio/ArgProg/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Permitir sólo el acceso desde el puerto 4200
+        config.setAllowedOrigins(Arrays.asList("/**")); 
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         config.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
