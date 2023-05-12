022d2ec8f862d3f6b6f42f9c344ef41ec16d4794
askra <askrajpr@gmail.com>
2023-05-12 08:35:35 -0300

    cors update 2.0

--- src/main/java/com/miporftolio/ArgProg/config/SecurityConfiguration.java
+++ src/main/java/com/miporftolio/ArgProg/config/SecurityConfiguration.java
@@ -1,32 +1,25 @@
 package com.miporftolio.ArgProg.config;
 
-import com.miporftolio.ArgProg.filters.JwtRequestFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
+   
+   @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Autowired
-    private JwtRequestFilter jwtRequestFilter;
-    
-   
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-             .cors().and()
             .authorizeHttpRequests((authorize) -> authorize
                 .anyRequest().permitAll()
             )
             .csrf().disable()
             .httpBasic().disable()
             .formLogin().disable();
-              return http.build();
+        return http.build();
     }
 }
